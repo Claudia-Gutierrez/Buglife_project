ef62c64ca85b52419bba82d13e4bf003da345f01
Claudia-Gutierrez <gutierrez.arellano.c@gmail.com>
2022-02-28 10:49:39 +0000

    st label

--- condatis_test.R
+++ condatis_test.R
@@ -1,6 +1,11 @@
+library(raster)
+library(sf)
+library(rgdal)
+library(dplyr)
+library(maptools)
 
 hab<- raster("data/raster/test.tif")
 st<- raster("data/raster/st.tif")
 
-
 test<-Condatis(hab=hab, st=st, R=100, powerthresh=0.25, disp=1)
+ 
