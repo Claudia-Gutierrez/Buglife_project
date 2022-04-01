6c1c0d2c694153c080fdf065bd0342c3215a1832
Claudia-Gutierrez <49790395+Claudia-Gutierrez@users.noreply.github.com>
2022-04-01 15:40:42 +0100

    layers for condatis

--- layers_prep.R
+++ layers_prep.R
@@ -2,7 +2,7 @@ library (rgdal)
 library(raster)
 library(terra)
 
-#######LANDSCAPE WITHOUT 'GET CUMBRIA BUZZING' PROJECTS#############
+#######LANDSCAPE CUMBRIA NATURAL RECOVERY HABITATS#############
 
 #Read original habitat shapefile
 Cumbriahab <- readOGR("spatialdata", "CumbriaHab")
@@ -37,6 +37,7 @@ hab_qual<-reclassify(CHR, qual) #replace habitat ID with quality value
 crs (hab_qual)<-"EPSG:27700"
 writeRaster(hab_qual,"spatialdata/habitat.tif", overwrite=TRUE)
 
+plot(hab_qual)
 
 #######LANDSCAPE INCLUDING 'GET CUMBRIA BUZZING (GCB)' PROJECTS#############
 
@@ -48,20 +49,48 @@ GCB$qual<- 1 #add a column with the habitat quality value
 ## Rasterize the shapefile with the raster "template" (r) used above
 GCBr <- rasterize(GCB, r,'qual')
 crs (GCBr)<-"EPSG:27700"
+
 plot(GCBr)
 
 ###Add GCBr to hab_qual (habitat.tif)
 
-
 hab_bl<- raster:::.ifel(GCBr > 0, 1,hab_qual)#if GCBr==1 then change value in hab_qual to 1
+
+crs (hab_bl)<-"EPSG:27700"
+writeRaster(hab_bl,"spatialdata/habitatBL.tif", overwrite=TRUE)
 
 plot(hab_bl)
 
+###########CREATE SOURCE AND TARGET RASTER#############
 
+st <- raster(ext, res=gridsize) #extent=aoi@bbox, gridsize defined for habitat 
+st[]<-NA
 
+#movement SOUTH to NORTH
+st@nrows
+st1<- as.matrix(sandt)
+st1[1:2,]<-2
+st1[399:400, ]<-1 # st@nrows-1:st@nrows   
+ 
+# #movement NORTH TO SOUTH
+# st1<- as.matrix(sandt)
+# st1[1:2,]<-1
+# st1[399:400, ]<-2 # st@nrows-1:st@nrows   
+# #movement WEST TO EAST
+#st@ncols
+# st1<- as.matrix(sandt)
+# st1[,1:2]<-1
+# st1[,491:492]<-2 # st@ncols-1:st@ncols 
+# #movement EAST TO WEST
+# st1<- as.matrix(sandt)
+# st1[,1:2]<-1
+# st1[,491:492]<-2 # st@ncols-1:st@ncols 
 
+st[]<-st1
 
+plot(st)
 
-
+crs (st)<-"EPSG:27700"
+writeRaster(st,"spatialdata/st.tif", overwrite=TRUE)
 
 
